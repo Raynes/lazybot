9ef16909547a253a22c3dc39caa66771e6d0ef0e
Rayne <DiscipleRayne@gmail.com>
2010-03-22 15:35:34 -0500

    Updated code for the new versions of clj-sandbox. Clojure code evaluation is working fine now.

--- src/sexpbot/plugins/eval.clj
+++ src/sexpbot/plugins/eval.clj
@@ -1,25 +1,35 @@
 (ns sexpbot.plugins.eval
   (:use net.licenser.sandbox
-	(sexpbot respond commands)))
+	(sexpbot respond commands))
+  (:import java.io.StringWriter
+	   java.util.concurrent.TimeoutException))
 
 (def eval-cmds
      {"eval" :eval})
 
 (enable-security-manager)
-(def sc (create-sandbox-compiler 'namespace 
-				 *default-sandbox-tester* 
-				 10000))
+
+(def sandbox-tester
+     (extend-tester secure-tester 
+		    (whitelist (function-matcher 'println 'print 'byte 'into-array))
+		    (whitelist (class-matcher String Byte Character Double Float Integer Long
+					      Math Number Object Short StrictMath StringBuffer))))
+
+(def sc (stringify-sandbox (new-sandbox-compiler :tester sandbox-tester :timeout 10000)))
 
 (def cap 100)
 
 (defn trim [s]
-  (apply str (take cap s)))
+  (let [res (.replaceAll (apply str (take cap s)) "\n" " ")]
+    (if (= (count res) 100) (str res "...") res)))
 
 (defn execute-text [txt]
-  (try
-   (trim (str ((sc txt) {})))
-   (catch SecurityException _ "DENIED!")
-   (catch Exception e (.getMessage (.getCause e)))))
+  (let [writer (StringWriter.)]
+    (try
+     (trim (str writer ((sc txt) {'*out* writer})))
+     (catch TimeoutException _ "Execution Timed Out!")
+     (catch SecurityException _ "DENIED!")
+     (catch Exception e (.getMessage (.getCause e))))))
 
 (defmethod respond :eval [{:keys [bot channel command args]}]
   (.sendMessage bot channel (->> args (interpose " ") (apply str) execute-text)))
