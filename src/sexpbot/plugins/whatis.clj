69783f94bad9a6a28e942537146b2fefcff88395
Rayne <DiscipleRayne@gmail.com>
2010-03-27 20:14:12 -0500

    Added a way to delete database entries in whatis.clj, and added a function to decode a file as a map ALWAYS even if it's empty to utilities.clj

--- src/sexpbot/plugins/whatis.clj
+++ src/sexpbot/plugins/whatis.clj
@@ -5,25 +5,37 @@
 
 (def whatis (str (System/getProperty "user.home") "/.sexpbot/whatis.json"))
 
+(defn if-exists-decode [file]
+  (into {} 
+	(if (.exists (File. file))
+	  (json/decode-from-reader (FileReader. file))
+	  nil)))
+
 (defmethod respond :put [{:keys [bot channel args]}]
   (let [[subject & is] args
-	current (if (.exists (File. whatis)) 
-		  (seq (json/decode-from-reader (FileReader. whatis)))
-		  nil)]
+	current (if-exists-decode whatis)]
     (.flush (json/encode-to-writer 
-	     (merge (into {} current) 
-		    {(keyword subject) (apply str (interpose " " is))}) 
+	     (merge current 
+		    {subject (apply str (interpose " " is))}) 
 	     (FileWriter. whatis)))
     (.sendMessage bot channel (str subject " = " (apply str (interpose " " is))))))
 
 (defmethod respond :get [{:keys [bot channel args]}]
-  (let [whatmap (if (.exists (File. whatis))
-		  (seq (json/decode-from-reader (FileReader. whatis)))
-		  nil)
-	result (->> (first args) keyword ((into {} whatmap)))]
+  (let [whatmap (if-exists-decode whatis)
+	result (->> (first args) keyword whatmap)]
     (if result
-      (.sendMessage bot channel (str (first args) " = " result)))))
+      (.sendMessage bot channel (str (first args) " = " result))
+      (.sendMessage bot channel (str (first args) " does not exist in my database.")))))
+
+(defmethod respond :delete [{:keys [bot channel args]}]
+  (let [whatmap (if-exists-decode whatis)
+	subject (keyword (first args))]
+    (if (whatmap subject) 
+      (do (json/encode-to-writer (dissoc whatmap subject) (FileWriter. whatis))
+	  (.sendMessage bot channel (str subject " is removed. RIP.")))
+      (.sendMessage bot channel (str subject " is not in my database.")))))
 
 (defmodule :whatis
-  {"put" :put
-   "get" :get})
+  {"put"    :put
+   "get"    :get
+   "delete" :delete})
--- src/sexpbot/utilities.clj
+++ src/sexpbot/utilities.clj
@@ -1,4 +1,12 @@
-(ns sexpbot.utilities)
+(ns sexpbot.utilities
+  (:require [org.danlarkin.json :as json])
+  (:import (java.io File FileReader)))
 
 (defn stringify [coll]
   (apply str (interpose " " coll)))
+
+(defn if-exists-decode [file]
+  (into {} 
+	(if (.exists (File. file))
+	  (json/decode-from-reader (FileReader. file))
+	  nil)))
