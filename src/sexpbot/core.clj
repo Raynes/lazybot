ae8f4bc0017e6bc869167c6a7e5284ed92a11aa7
Rayne <DiscipleRayne@gmail.com>
2010-03-31 17:42:48 -0500

    Added a shitty leetspeek plugin.

--- src/sexpbot/core.clj
+++ src/sexpbot/core.clj
@@ -1,9 +1,11 @@
 (ns sexpbot.core
-  (:use (sexpbot.plugins utils eball google lmgtfy translate eval whatis dynamic)
+  (:use (sexpbot.plugins utils eball google lmgtfy translate 
+			 eval whatis dynamic leet)
 	sexpbot.respond
 	[clojure.contrib.str-utils :only [re-split]])
+  (:require [org.danlarkin.json :as json])
   (:import (org.jibble.pircbot PircBot)
-	   java.io.File))
+	   (java.io File FileReader)))
 
 (def prepend \$)
 (def server "irc.freenode.net")
@@ -21,22 +23,33 @@
 			:args args}))
 
 (when (not (.exists sexpdir)) (.mkdir sexpdir))
+(.createNewFile (File. (str (str sexpdir) "/privileges.clj")))
+
+;;; Possible future privilege system ;;;
+(defn get-priv [user]
+  ((->> "/privileges.clj" 
+	(str sexpdir) 
+	FileReader. 
+	json/decode-from-reader 
+	(into {})) (keyword user)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn make-bot [] 
   (let [bot (proxy [PircBot] []
 	      (onMessage 
 	       [chan send login host mess]
-	       (if (= (first mess) prepend)
-		 (respond (merge (split-args (apply str (rest mess)))
-				 {:bot this 
-				  :sender send 
-				  :channel chan 
-				  :login login 
-				  :host host})))))]
+	       (let [bot-map
+		     {:bot this
+		      :sender send
+		      :channel chan
+		      :login login
+		      :host host}]
+		 (if (= (first mess) prepend)
+		   (respond (merge (split-args (apply str (rest mess))) bot-map))))))]
     (wall-hack-method PircBot :setName [String] bot "sexpbot")
     (doto bot
       (.setVerbose true)
       (.connect server))
     (doseq [chan channels] (.joinChannel bot chan))))
 
-(make-bot) 
+(make-bot)
---
+++ src/sexpbot/plugins/leet.clj
@@ -0,0 +1,31 @@
+(ns sexpbot.plugins.leet
+  (:use (sexpbot commands respond)))
+
+(defn char-to-leet [c]
+  (condp = c
+    \e 3
+    \a 4
+    \b 8
+    \g 9
+    \i \!
+    \l 1
+    \o 0
+    \q 9
+    \s 5
+    \t 7
+    \z 2
+    c))
+
+(defn leetspeek [s]
+  (->> s (#(.toLowerCase %)) (map char-to-leet) (apply str)))
+
+(defmethod respond :elite [{:keys [bot channel args]}]
+  (.sendMessage bot channel 
+		(->> args 
+		     (interpose " ")
+		     (apply str)
+		     (#(.toLowerCase %))
+		     leetspeek)))
+
+(defmodule :leet
+  {"elite" :elite})
