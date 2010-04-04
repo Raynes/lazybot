1f342a14eefca1b2070b8ee5e97e27bc000e8049
Rayne <DiscipleRayne@gmail.com>
2010-04-04 13:15:11 -0500

    Fixed weather bug. Now works for all countries supported by wunderground.

--- src/sexpbot/plugins/weather.clj
+++ src/sexpbot/plugins/weather.clj
@@ -7,19 +7,28 @@
 (def forcasturl "http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=")
 
 (defn cull [zipper]
-  [(first (zf/xml-> zipper :txt_forecast :date zf/text)) 
-   (zf/xml-> zipper :txt_forecast :forecastday :fcttext zf/text)])
+  (let [[date & more] (zf/xml-> zipper :txt_forecast :date zf/text)
+	[pdate & more] (zf/xml-> zipper :simpleforecast :forecastday :date :pretty_short zf/text)
+	ftext (zf/xml-> zipper :txt_forecast :forecastday :fcttext zf/text)
+	[hi & more] (zf/xml-> zipper :simpleforecast :forecastday :high :celsius zf/text)
+	[low & more] (zf/xml-> zipper :simpleforecast :forecastday :low :celsius zf/text)
+	[condition & more] (zf/xml-> zipper :simpleforecast :forecastday :conditions zf/text)]
+    (if (seq date)
+      [date ftext]
+      [pdate (str "High: " hi " Low: " low " Conditions: " condition)])))
 
 (defn get-fcst [query]
   (->> query (remove #(= \space %)) (apply str) (str forcasturl) xml/parse zip/xml-zip cull))
 
 (defmethod respond :fcst [{:keys [bot channel sender args]}]
-  (let [[date [today tonight]] (->> args (remove #(= \space %)) get-fcst)]
+  (let [[date [today tonight :as a]] (->> args (remove #(= \space %)) get-fcst)
+	conditions (if (string? today) today a)]
     (if (seq date)
       (do
 	(.sendMessage bot channel (str sender ": " date))
-	(.sendMessage bot channel (str sender ": TODAY: " today))
-	(.sendMessage bot channel (str sender ": TONIGHT: " tonight)))
+	(.sendMessage bot channel (str sender ": TODAY: " conditions))
+	(when (string? today)
+	  (.sendMessage bot channel (str sender ": TONIGHT: " tonight))))
       (.sendMessage bot channel (str sender ": Location not found!")))))
 
 (defmodule :weather
