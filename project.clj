79b7a7016ec3dd08d769e2449bccea9844ab3edc
Rayne <DiscipleRayne@gmail.com>
2010-04-15 18:20:38 -0500

    Removed pircbot dep.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-		 [pircbot/pircbot "1.4.2"]
 		 [commons-lang/commons-lang "2.5"]
 		 [commons-io/commons-io "1.4"]
 		 [org.danlarkin/clojure-json "1.1-SNAPSHOT"]
