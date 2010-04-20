e310a4409b4d09184986e604b04d7b739c04dd37
Rayne <DiscipleRayne@gmail.com>
2010-04-19 22:40:46 -0500

    Added a way to get examples directly in IRC using Walton.

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
 		 [org.clojars.rayne/clj-time "0.1.0-SNAPSHOT"]
 		 [org.clojars.rayne/clj-sandbox "0.3.0-1.2-SNAPSHOT"]
 		 [org.clojars.rayne/clj-apache-http "2.0.1"]
-		 [irclj "0.2.0-SNAPSHOT"]]
+		 [irclj "0.2.0-SNAPSHOT"]
+		 [walton "0.5.0"]]
   :dev-dependencies [[swank-clojure "1.1.0"]
 		     [leiningen/lein-swank "1.1.0"]
 		     [lein-search "0.3.0-SNAPSHOT"]])
--- src/sexpbot/plugins/walton.clj
+++ src/sexpbot/plugins/walton.clj
@@ -1,6 +1,9 @@
 (ns sexpbot.plugins.walton
   (:use [sexpbot respond])
-  (:require [irclj.irclj :as ircb]))
+  (:require [irclj.irclj :as ircb])
+  (:use walton.core))
+
+(background-init-walton)
 
 (def wurl "http://getclojure.org:8080/examples/")
 
@@ -9,5 +12,10 @@
 (defmethod respond :walton [{:keys [irc channel nick args]}]
   (ircb/send-message irc channel (->> args first construct-url (str nick ": "))))
 
+(defmethod respond :example [{:keys [irc channel args]}]
+  (let [[example result] (walton (first args))]
+    (ircb/send-message irc channel (str example " => " result))))
+
 (defplugin
-  {"walton" :walton})
+  {"walton"  :walton
+   "example" :example})
