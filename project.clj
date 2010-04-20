6d0fc1f2c00e59c959ab75d6da89b2c083152578
Rayne <DiscipleRayne@gmail.com>
2010-04-20 14:29:35 -0500

    Updated clj-sandbox dep.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 		 [commons-io/commons-io "1.4"]
 		 [org.danlarkin/clojure-json "1.1-SNAPSHOT"]
 		 [org.clojars.rayne/clj-time "0.1.0-SNAPSHOT"]
-		 [org.clojars.rayne/clj-sandbox "0.3.0-1.2-SNAPSHOT"]
+		 [clj-sandbox "0.3.0"]
 		 [org.clojars.rayne/clj-apache-http "2.0.1"]
 		 [irclj "0.2.0-SNAPSHOT"]
 		 [walton "0.5.0"]]
