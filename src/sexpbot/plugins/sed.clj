b9065556640554145252d03b02da84ebd97ce447
Erik Price <erik.price16@gmail.com>
2010-04-25 20:53:01 -0400

    Some more minor changes

--- src/sexpbot/plugins/sed.clj
+++ src/sexpbot/plugins/sed.clj
@@ -11,8 +11,8 @@
 ;;;     OPTIONS       ;;;
 ;; case insensitive -i ;;
 ;; don't execute -x    ;;
-;; --sed a specific usr;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn parse-opts [opt-string]
   {:case-inse (.contains opt-string "i")
    :no-exec (.contains opt-string "x")})
@@ -26,7 +26,7 @@
 	last-in (:last-in @irc)
 	opts (parse-opts (if (re-find #"([a-zA-Z]+)[\s]+s/([^/]+)/([^/]*)/" conj-args)
 			   (rest (re-matches #"([a-zA-Z]+)[\s]+s/[^/]+/[^/]*/" conj-args))
-			   " "))] ;; NAIVE, expects correct input
+			   " "))]
     (when (= last-in "")
       (ircb/send-message irc channel "No one said anything yet!"))
     (when (re-matches #"([a-zA-Z]*)[\s]+s/[^/]+/[^/]*/" conj-args)
