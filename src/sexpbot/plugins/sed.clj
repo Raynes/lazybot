8fd6bef420e991e27d89be374163b4303b64e88d
Erik Price <erik.price16@gmail.com>
2010-05-01 18:47:41 -0400

    Added shorthand notation for Sed, fixed an unhandled exception

--- src/sexpbot/plugins/sed.clj
+++ src/sexpbot/plugins/sed.clj
@@ -10,13 +10,18 @@
 Try $help sed")))
     
 (defn sed [string regexp replacement]
-    (.replaceAll string (str "(?i)" "" regexp) replacement))
+  (try
+   (.replaceAll string (str "(?i)" regexp) replacement)
+   (catch java.util.regex.PatternSyntaxException e (str "Incorrectly formatted regular expression: " regexp))))
 
 (defplugin
   (:add-hook :on-message
 	     (fn [{:keys [irc nick message channel] :as irc-map}]
+	       (when (not-empty (re-find #"s/([^/]+)/([^/]*)/" message))
+		 (try-handle (assoc irc-map :message (str prepend "sed " message))))
+				
 	       (when (and (not= nick (:name @irc))
-			  (not= (take 4 message) (cons (:prepend (read-config)) "sed")))
+			  (not= (take 4 message) (cons prepend "sed")))
 		 (dosync
 		  (alter message-map assoc-in [irc channel nick] message)
 		  (alter message-map assoc-in [irc channel :channel-last] message )))))
@@ -24,7 +29,8 @@ Try $help sed")))
   (:sed 
    "Simple find and replace. Usage: sed [-<user name>] s/<regexp>/<replacement>/
     If the specified user isn't found, it will default to the last thing said in the channel.
-    Example Usage: sed -boredomist s/[aeiou]/#/"
+    Example Usage: sed -boredomist s/[aeiou]/#/
+    Shorthand    : s/[aeiou]/#/"
    ["sed"]
    [{:keys [irc channel args] :as irc-map}]
    (let [farg (or (first args) "")
@@ -45,9 +51,7 @@ Try $help sed")))
 		       NullPointerException e nil)))
 	 [regexp replacement] (or
 			       (not-empty (rest (re-find #"s/([^/]+)/([^/]*)/" margs)))
-			       nil)]
-	 ;; Options temporarily removed
-     
+			       nil)]     
      (cond
       (empty? last-in) (ircb/send-message irc channel "No one said anything yet!")
       (not-any? seq [regexp replacement]) (format-msg irc-map)
