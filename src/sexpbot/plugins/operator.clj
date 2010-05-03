d1b9398a97ef70d42aaa1c50ee4f4c84a03e4ec1
Rayne <DiscipleRayne@gmail.com>
2010-05-03 08:50:12 -0500

    * src/sexpbot/plugins/operator.clj: Made this plugin compatible with the new if-admin stuff.

--- src/sexpbot/plugins/operator.clj
+++ src/sexpbot/plugins/operator.clj
@@ -6,47 +6,47 @@
   (:op 
    "Sets the person you specify as operator. ADMIN ONLY."
    ["op"] 
-   [{:keys [irc nick channel args]}]
-   (if-admin nick (ircb/set-mode irc channel "+o" (first args))))
+   [{:keys [irc nick channel args] :as irc-map}]
+   (if-admin nick irc-map (ircb/set-mode irc channel "+o" (first args))))
   
   (:deop 
    "Deops the person you specify. ADMIN ONLY."
    ["deop"] 
-   [{:keys [irc nick channel args]}]
-   (if-admin nick (ircb/set-mode irc channel  "-o" (first args))))
+   [{:keys [irc nick channel args] :as irc-map}]
+   (if-admin nick irc-map (ircb/set-mode irc channel  "-o" (first args))))
 
   (:kick 
    "Kicks the person you specify. ADMIN ONLY."
    ["kick"] 
-   [{:keys [irc nick channel args]}]
-   (if-admin nick (ircb/kick irc channel (first args) :reason (apply str (rest args)))))
+   [{:keys [irc nick channel args] :as irc-map}]
+   (if-admin nick irc-map (ircb/kick irc channel (first args) :reason (apply str (rest args)))))
 
   (:settopic 
    "Set's the channel's topic. ADMIN ONLY."
    ["settopic"]
-   [{:keys [irc nick channel args]}]
-   (if-admin nick (ircb/set-topic irc channel (apply str (interpose " " args)))))
+   [{:keys [irc nick channel args] :as irc-map}]
+   (if-admin nick irc-map (ircb/set-topic irc channel (apply str (interpose " " args)))))
 
   (:ban 
    "Ban's whatever you specify. ADMIN ONLY."
    ["ban"]
-   [{:keys [irc nick channel args]}]
-   (if-admin nick (ircb/set-mode irc channel "+b" (first args))))
+   [{:keys [irc nick channel args] :as irc-map}]
+   (if-admin nick irc-map (ircb/set-mode irc channel "+b" (first args))))
 
   (:unban 
    "Unban's whatever you specify. ADMIN ONLY."
    ["unban"]
-   [{:keys [irc nick channel args]}]
-   (if-admin nick (ircb/set-mode irc channel "-b" (first args))))
+   [{:keys [irc nick channel args] :as irc-map}]
+   (if-admin nick irc-map (ircb/set-mode irc channel "-b" (first args))))
 
   (:voice 
    "Voices the person you specify. ADMIN 0NLY."
    ["voice"] 
-   [{:keys [irc channel nick args]}]
-   (if-admin nick (ircb/set-mode irc channel "+v" (first args))))
+   [{:keys [irc channel nick args] :as irc-map}]
+   (if-admin nick irc-map (ircb/set-mode irc channel "+v" (first args))))
 
   (:devoice 
    "Devoices the person you specify. ADMIN ONLY."
    ["devoice"]
-   [{:keys [irc channel nick args]}]
-   (if-admin nick (ircb/set-mode irc channel "-v" (first args)))))
+   [{:keys [irc channel nick args] :as irc-map}]
+   (if-admin nick irc-map (ircb/set-mode irc channel "-v" (first args)))))
