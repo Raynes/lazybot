01a66c821157f213eeba2594e029b44997106815
Rayne <DiscipleRayne@gmail.com>
2010-05-03 08:51:17 -0500

    * src/sexpbot/plugins/seen.clj: Made seen server-local. You cannot $seen somebody on another server. This way one person with a nickname that somebody else has on another server the bot is running will not be considered the same.

--- src/sexpbot/plugins/seen.clj
+++ src/sexpbot/plugins/seen.clj
@@ -1,6 +1,6 @@
 (ns sexpbot.plugins.seen
   (:refer-clojure :exclude [extend])
-  (:use [sexpbot respond info utilities]
+  (:use [sexpbot respond info]
 	[clj-time core format]
 	stupiddb.core)
   (:require [irclj.irclj :as ircb]))
@@ -9,27 +9,26 @@
 (def seenfile (str sexpdir "/seen.db"))
 (def db (db-init seenfile 1800))
 
-
 (defn tack-time
   "Takes a nick and updates the seen database with that nick and the current time."
-  [nick channel doing]
+  [nick server channel doing]
   (let [lower-nick (.toLowerCase nick)]
-    (db-assoc db lower-nick {:time (unparse (formatters :date-time) (now)) 
-			     :chan channel 
-			     :doing doing
-			     :nick nick}))
-  (flush-db db))
+    (db-assoc-in db [server lower-nick] 
+		 {:time (unparse (formatters :date-time) (now)) 
+		  :chan channel 
+		  :doing doing
+		  :nick nick})))
 
 
 (defn get-seen
   "Get's the last-seen for a nick."
-  [nick]
-  (when-let [seen-map (db-get db (.toLowerCase nick))]
+  [nick server]
+  (when-let [seen-map (db-get-in db [server (.toLowerCase nick)])]
     (assoc seen-map :time (in-minutes 
 			   (interval (parse (formatters :date-time) (:time seen-map))
 				     (now))))))
 
-(defn put-seen [{:keys [nick channel]} doing] (tack-time nick channel doing))
+(defn put-seen [{:keys [nick channel irc]} doing] (tack-time nick (:server @irc) channel doing))
 
 (defplugin
   (:add-hook :on-message
@@ -44,7 +43,7 @@
    "Checks to see when the person you specify was last seen."
    ["seen"] 
    [{:keys [irc nick channel args]}]
-   (if-let [{:keys [time chan doing nick]} (get-seen (first args))]
+   (if-let [{:keys [time chan doing nick]} (get-seen (first args) (:server @irc))]
      (ircb/send-message irc channel (str nick " was last seen " doing (when-not (= doing "quitting") " on ") 
 					 chan " " time " minutes ago."))
      (ircb/send-message irc channel (str "I have never seen " (first args) "."))))
