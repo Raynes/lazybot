195cb8aadc0b7c65b19d9e4a199caaba6c755d8e
Rayne <DiscipleRayne@gmail.com>
2010-05-03 19:42:32 -0500

    * src/sexpbot/plugins/login.clj: Wrong number of args passed to read-config.

--- src/sexpbot/plugins/login.clj
+++ src/sexpbot/plugins/login.clj
@@ -12,7 +12,7 @@
 
 (defplugin
   (:add-hook :on-quit
-	    (fn [irc-map] (try-handle (assoc irc-map :message (str (:prepend (read-config)) "quit")))))
+	    (fn [irc-map] (try-handle (assoc irc-map :message (str (:prepend (read-config info-file)) "quit")))))
 
   (:login 
    "Best executed via PM. Give it your password, and it will log you in."
