eb9629f2ca102649d20518b308c6c512d8111c51
Rayne <DiscipleRayne@gmail.com>
2010-05-04 18:20:40 -0500

    * src/sexpbot/plugins/status.clj: Fixed 'er up.

--- src/sexpbot/plugins/status.clj
+++ src/sexpbot/plugins/status.clj
@@ -10,19 +10,21 @@
    ["setaway"]
    [{:keys [irc nick channel args] :as irc-map}]
    (dosync
-    (alter statusmsg-map assoc irc {nick {:status :away :msg (let [msg (.trim
-									(->> args
-									     (interpose " ")
-									     (apply str)))]
-							       (if (seq msg)
-								 msg
-								 "I'm away."))}})))  
+    (alter statusmsg-map assoc-in [(:server @irc) nick]
+	   {:status :away 
+	    :msg (let [msg (.trim
+			    (->> args
+				 (interpose " ")
+				 (apply str)))]
+		   (if (seq msg)
+		     msg
+		     "I'm away."))})))
    (:return
     "Return from being away."
     ["return"]
     [{:keys [irc nick channel] :as irc-map}]
     (dosync
-     (alter statusmsg-map assoc irc {nick {:status :active :msg ""}})))
+     (alter statusmsg-map assoc (:server @irc) {nick {:status :active :msg ""}})))
 
    (:status
     "Get the status of a user."
@@ -30,10 +32,10 @@
     [{:keys [irc nick channel args] :as irc-map}]
     (let [user (.trim (or (first args) ""))]
       (if (seq user)
-	(let [status-map (get (get @statusmsg-map irc) user)]
-	  (if status-map
+	(if-let [status-m (@statusmsg-map (:server @irc))]
+	  (let [status-map (status-m user)]
 	    (if (= :away (get status-map :status))
 	      (ircb/send-message irc channel (str nick " is away: " (status-map :msg)))
-	      (ircb/send-message irc channel (str nick " is active. ")))
-	    (ircb/send-message irc channel (str (first args) " doesn't exist, or hasn't set their status"))))
+	      (ircb/send-message irc channel (str nick " is active. "))))
+	  (ircb/send-message irc channel (str (first args) " doesn't exist, or hasn't set their status")))
 	(ircb/send-message irc channel "Who?")))))
