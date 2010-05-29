10c5f4bd233a2a80b8b4c7cc4c5a25b744c2fc2d
Raynes <DiscipleRayne@gmail.com>
2010-05-29 05:49:12 -0500

    * src/sexpbot/plugins/haskell.clj: Fixed Haskell stuff.

--- src/sexpbot/plugins/haskell.clj
+++ src/sexpbot/plugins/haskell.clj
@@ -4,19 +4,21 @@
 	    [clojure-http.resourcefully :as res]
 	    [irclj.irclj :as ircb]))
 
-(def tryurl "http://tryhaskell.org/haskell.json?method=eval")
+(def tryurl "http://tryhaskell.org/haskell.json")
+
+(defn cull [js]
+  (if-let [result (seq (:result js))] result (:error js)))
 
 (defn eval-haskell [expr]
-  (-> (res/get tryurl {} {"expr" expr}) 
+  (-> (res/get tryurl {} {"method" "eval" "expr" expr}) 
       :body-seq
       first
       json/decode-from-str
-      :result))
+      cull))
 
 (defplugin
   (:heval 
    "Evaluates some Haskell code. Doesn't print error messages and uses the TryHaskell API."
    ["heval"] 
    [{:keys [irc channel nick args]}]
-   (ircb/send-message irc channel 
-		      (str nick ": " (eval-haskell (apply str (interpose " " args)))))))
+   (ircb/send-message irc channel (str "\"" (eval-haskell (apply str (interpose " " args))) "\""))))
