b7586f6d370a1aab786942d9861483f4bd6604a0
Raynes <DiscipleRayne@gmail.com>
2010-07-22 01:40:22 -0500

    Updated dependencies for clojure 1.2-beta1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject sexpbot "0.5.0"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
+                 [org.clojure/clojure-contrib "1.2.0-beta1"]
 		 [commons-lang/commons-lang "2.5"]
 		 [commons-io/commons-io "1.4"]
 		 [org.danlarkin/clojure-json "1.1"]
@@ -10,8 +10,7 @@
 		 [clojure-http-client "1.1.0-SNAPSHOT"]
 		 [irclj "0.3.0-SNAPSHOT"]
 		; [walton "0.5.0"]
-		 [stupiddb "0.3.0"]
+		 [stupiddb "0.3.1"]
 		 [clj-config "0.1.0-SNAPSHOT"]
                  [clj-github "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-		     [lein-search "0.3.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]])
