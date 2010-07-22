c7ec563bbed416ba19eeb88cc16b7c1e13d526ca
Raynes <DiscipleRayne@gmail.com>
2010-07-22 02:47:53 -0500

    Updated deps.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 		 [commons-io/commons-io "1.4"]
 		 [org.danlarkin/clojure-json "1.1"]
 		 [org.clojars.rayne/clj-time "0.1.0-SNAPSHOT"]
-		 [clj-sandbox "0.4.0"]
+		 [clj-sandbox "0.4.1"]
 		 [clojure-http-client "1.1.0-SNAPSHOT"]
 		 [irclj "0.3.0-SNAPSHOT"]
 		; [walton "0.5.0"]
