5681f6a811ad61fce418e12094da45e36e311c31
Raynes <DiscipleRayne@gmail.com>
2010-09-14 12:32:48 -0500

    Added Haskell evaluation and a little type checking feature powered by mueval and ghc -e respectively.

--- src/sexpbot/plugins/haskell.clj
+++ src/sexpbot/plugins/haskell.clj
@@ -1,7 +1,8 @@
 (ns sexpbot.plugins.haskell
   (:use [sexpbot respond]
         [clojure.contrib.json :only [read-json]]
-	[clojure-http.client :only [add-query-params]])
+	[clojure-http.client :only [add-query-params]]
+        [clojure.java.shell :only [sh]])
   (:require [clojure-http.resourcefully :as res]))
 
 (def tryurl "http://tryhaskell.org/haskell.json")
@@ -17,9 +18,26 @@
        cull
        (apply str)))
 
+(defn mueval [expr]
+  (:out (sh "mueval" "-e" expr)))
+
 (defplugin
   (:cmd
    "Evaluates some Haskell code. Doesn't print error messages and uses the TryHaskell API."
-   #{"heval"} 
-   (fn [{:keys [irc bot channel nick args]}]
-     (send-message irc bot channel (str "=> " (eval-haskell (apply str (interpose " " args))))))))
+   #{"tryhaskell"} 
+   (fn [{:keys [irc bot channel args]}]
+     (send-message irc bot channel (str "=> " (eval-haskell (apply str (interpose " " args)))))))
+
+  (:cmd
+   "Evaluates Haskell code with mueval."
+   #{"heval" "he"}
+   (fn [{:keys [irc bot channel args]}]
+     (let [result (mueval (apply str (interpose " " args)))]
+       (send-message irc bot channel (str "=> " (if (> 200 (count result)) result (str result "...")))))))
+
+  (:cmd
+   "Gets the type of an expression via GHC's :t."
+   #{"htype" "ht"}
+   (fn [{:keys [irc bot channel args]}]
+     (send-message irc bot channel
+                   (str "Type: " (->> args (interpose " ") (apply str) (str ":t ") (sh "ghc" "-e") :out))))))
