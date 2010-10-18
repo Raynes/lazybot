1edc96555131cf8097f7217c100db20becf440ae
Raynes <DiscipleRayne@gmail.com>
2010-10-18 14:34:46 -0500

    A better logging plugin (written by ninjudd, fixed by Rayne).

---
+++ src/sexpbot/plugins/logger.clj
@@ -0,0 +1,22 @@
+(ns sexpbot.plugins.logger
+  (:use [sexpbot respond]
+        [clj-time.core :only [now]]
+        [clj-time.format :only [unparse formatters]]
+        [clojure.java.io :only [file]])
+  (:import [java.io File]))
+
+(defplugin
+  (:hook
+   :on-message
+   (fn [{:keys [irc bot nick channel message]}]
+     (let [config (:config @bot)
+           server (:server @irc)]
+       (if (get-in config [server :log channel])
+         (let [log-dir  (file (:log-dir (config server)) server channel)
+               date     (unparse (formatters :date) (now))
+               time     (unparse (formatters :time) (now))
+               log-file (file log-dir (str date ".txt"))]
+           (.mkdirs log-dir)
+           (spit log-file
+                 (format "%s: %s: %s\n" time nick message)
+                 :append true)))))))
