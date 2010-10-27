fd94999f686596d3feb51559a5d8e690b9551925
Michael D. Ivey <ivey@gweezlebur.com>
2010-10-26 20:12:43 -0500

    Add inc and dec commands to match (inc) and (dec)

--- src/sexpbot/plugins/karma.clj
+++ src/sexpbot/plugins/karma.clj
@@ -6,7 +6,7 @@
   (:use [sexpbot respond info]
 	[somnium.congomongo :only [fetch-one insert! destroy!]]))
 
-(defn set-karma
+(defn- set-karma
   [nick server channel karma]
   (let [lower-nick (.toLowerCase nick)]
     (destroy! :karma {:nick lower-nick :server server :channel channel})
@@ -16,8 +16,7 @@
               :karma karma
               :nick lower-nick})))
 
-(defn get-karma
-  "Gets the karma for a nick."
+(defn- get-karma
   [nick server channel]
   (let [lower-nick (.toLowerCase nick)
         user-map (fetch-one :karma :where {:nick lower-nick :server server :channel channel})]
@@ -25,7 +24,15 @@
       (:karma user-map)
       1)))
 
-(defn put-karma [{:keys [channel irc]} nick karma] (set-karma nick (:server @irc) channel karma))
+(defn- put-karma [{:keys [channel irc]} nick karma] (set-karma nick (:server @irc) channel karma))
+
+(defn- change-karma
+  [snick new-karma {:keys [nick irc bot channel] :as irc-map}]
+  (if (= nick snick)
+    (send-message irc bot channel "You can't adjust your own karma.")
+    (do
+      (put-karma irc-map snick new-karma)
+      (send-message irc bot channel (str "=> " new-karma)))))
 
 (defplugin
   (:hook :on-message
@@ -40,15 +47,27 @@
                                 "inc" (+ karma 1)
                                 "dec" (- karma 1)
                                 karma)]
-                 (if (= nick snick)
-                   (send-message irc bot channel "You can't adjust your own karma.")
-                   (do
-                     (put-karma irc-map snick new-karma)
-                     (send-message irc bot channel (str "=> " new-karma)))))))))
+                 (change-karma snick new-karma irc-map))))))
   (:cmd
    "Checks the karma of the person you specify."
    #{"karma"} 
    (fn [{:keys [irc bot nick channel args]}]
      (if-let [karma (get-karma (first args) (:server @irc) channel)]
        (send-message irc bot channel (str (first args) " has karma " karma "."))
-       (send-message irc bot channel (str "I have no record for " (first args) "."))))))
+       (send-message irc bot channel (str "I have no record for " (first args) ".")))))
+  (:cmd
+   "Increases the karma of the person you specify."
+   #{"inc"} 
+   (fn [{:keys [irc bot nick channel args] :as irc-map}]
+     (let [snick (first args)
+           karma (get-karma snick (:server @irc) channel)
+           new-karma (+ karma 1)]
+       (change-karma snick new-karma irc-map))))
+  (:cmd
+   "Decreases the karma of the person you specify."
+   #{"dec"}
+   (fn [{:keys [irc bot nick channel args] :as irc-map}]
+     (let [snick (first args)
+           karma (get-karma snick (:server @irc) channel)
+           new-karma (- karma 1)]
+       (change-karma snick new-karma irc-map)))))
