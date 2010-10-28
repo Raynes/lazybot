3e7163e7a9eac32f0738c6c23a77f20b39f7dbb1
Raynes <DiscipleRayne@gmail.com>
2010-10-27 19:54:44 -0500

    Timers now have IDs and are deletable.

--- src/sexpbot/plugins/timer.clj
+++ src/sexpbot/plugins/timer.clj
@@ -6,22 +6,22 @@
 
 (def running-timers (atom {}))
 
-(defn compose-timer [{:keys [end-time text]}]
+(defn cap-text [s n]
+  (str (apply str (take n s)) (if (< 30 (count s)) "..." "")))
+
+(defn compose-timer [[timer-n {:keys [end-time text]}]]
   (let [this-moment (now)
 	[ehours eminutes eseconds] [(hour this-moment) (minute this-moment) (sec this-moment)]
 	ftime (minus end-time (hours ehours) (minutes eminutes) (secs eseconds))
 	end-in ((juxt hour minute sec) ftime) ]
-    (str text " Ends in: " (apply str (interpose ":" end-in)))))
-
-(defn cap-text [s n]
-  (str (apply str (take n s)) (if (< 30 (count s)) "..." "")))
+    (str timer-n ": " (cap-text text 20) " ... Ends in: " (apply str (interpose ":" end-in)))))
 
 (defplugin
   (:cmd
    "Prints a list of the currently running timers. Execute in PM if you want a full list."
    #{"timers"} 
    (fn [{:keys [irc bot nick channel args]}]
-     (let [timers (map compose-timer (vals @running-timers))]
+     (let [timers (map compose-timer @running-timers)]
        (if (> (count timers) 0)
          (if-let [n-to-show (first args)]
            (doseq [timer (take n-to-show timers)] (send-message irc bot channel timer))
@@ -31,24 +31,31 @@
              (when (> (count timers) 3) (send-message irc bot channel "and more..."))))
          (send-message irc bot channel "No timers are currently running.")))))
 
+  (:cmd
+   "Deletes a timer."
+   #{"dl-timer"}
+   (fn [{:keys [irc bot channel args]}]
+     (swap! running-timers dissoc (Integer/parseInt (first args)))
+     (send-message irc bot channel "Timer deleted.")))
+
   (:cmd 
-   "Create's a timer. You specify the time you want it to run in a 00:00:00 format, and a message
+   "Creates a timer. You specify the time you want it to run in a 0:0:0 format, and a message
    to print once the timer has run. Once the timer completes, the message will be printed."
    #{"timer"} 
    (fn [{:keys [irc bot channel args]}]
-     (.start 
-      (Thread. 
-       (fn []
-         (let [ctime (now)
-               [hour minute sec] (map #(Integer/parseInt %) (.split (first args) ":"))
-               newt (plus ctime (hours hour) (minutes minute) (secs sec))
-               fint (in-secs (interval ctime newt))
-               text (->> args rest (interpose " ") (apply str))
-               timer-name (gensym)]
-           (swap! running-timers assoc timer-name {:end-time newt 
-                                                   :text (cap-text text 30)})
-           (Thread/sleep (* fint 1000))
-           (if (= (second args) "/me") 
-             (ircb/send-action irc channel (apply str (interpose " " (nnext args))))
-             (send-message irc bot channel text))
-           (swap! running-timers dissoc timer-name))))))))
+     (let [ctime (now)
+           [hour minute sec] (map #(Integer/parseInt %) (.split (first args) ":"))
+           newt (plus ctime (hours hour) (minutes minute) (secs sec))
+           fint (in-secs (interval ctime newt))
+           text (->> args rest (interpose " ") (apply str))
+           n-timers (-> @running-timers keys (or [0]) (->> (apply max)) inc)]
+       (future
+        (swap! running-timers assoc n-timers
+               {:end-time newt 
+                :text text})
+        (Thread/sleep (* fint 1000))
+        (when (@running-timers n-timers)
+          (if (= (second args) "/me") 
+            (ircb/send-action irc channel (apply str (interpose " " (nnext args))))
+            (send-message irc bot channel text)))
+        (swap! running-timers dissoc n-timers))))))
