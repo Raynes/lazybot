d08b6c13138bd2abb2045b6c551156b9f1b75054
Raynes <DiscipleRayne@gmail.com>
2010-10-28 19:04:00 -0500

    Now truncates text, and has a pretty arrow like in eval.

--- src/sexpbot/plugins/haskell.clj
+++ src/sexpbot/plugins/haskell.clj
@@ -2,13 +2,27 @@
   (:use [sexpbot respond]
         [clojure.contrib.json :only [read-json]]
 	[clojure-http.client :only [add-query-params]]
-        [clojure.java.shell :only [sh]])
+        [clojure.java.shell :only [sh]]
+        [clj-github.gists :only [new-gist]])
   (:require [clojure-http.resourcefully :as res]))
 
 (def tryurl "http://tryhaskell.org/haskell.json")
 
 (defn cull [js]
   (if-let [result (seq (:result js))] result (:error js)))
+
+(def cap 250)
+
+(defn trim [s]
+  (let [res (apply str (take cap s))
+	rescount (count res)]
+    (if (= rescount cap) 
+      (str res "... "
+           (when (> (count s) cap)
+             (try
+               (str "http://gist.github.com/" (:repo (new-gist {} "output.clj" s)))
+               (catch java.io.IOException _ nil))))
+      res)))
 
 (defn eval-haskell [expr]
   (->> (res/get (add-query-params tryurl {"method" "eval" "expr" expr})) 
@@ -26,14 +40,15 @@
    "Evaluates some Haskell code. Doesn't print error messages and uses the TryHaskell API."
    #{"tryhaskell"} 
    (fn [{:keys [irc bot channel args]}]
-     (send-message irc bot channel (str "=> " (eval-haskell (apply str (interpose " " args)))))))
+     (send-message irc bot channel (trim (str "=> " (eval-haskell (apply str (interpose " " args))))))))
 
   (:cmd
    "Evaluates Haskell code with mueval."
    #{"heval" "he"}
    (fn [{:keys [irc bot channel args]}]
      (let [result (mueval (apply str (interpose " " args)))]
-       (send-message irc bot channel (str "=> " (if (> 200 (count result)) result (str result "...")))))))
+       (send-message irc bot channel
+                     (str "\u27F9 " (trim (if (> 200 (count result)) result (str result "..."))))))))
 
   (:cmd
    "Gets the type of an expression via GHC's :t."
