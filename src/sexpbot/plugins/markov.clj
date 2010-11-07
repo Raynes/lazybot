04cf14cdac4bffc297e410a3af34fe702d9a6b1c
Alan Malloy <alan@malloys.org>
2010-11-07 00:28:07 -0700

    Alpha version of markov plugin; seems to be working

--- src/sexpbot/plugins/markov.clj
+++ src/sexpbot/plugins/markov.clj
@@ -32,55 +32,14 @@
 (defn trim-seq [s]
   (take-while identity s))
 
+(defn and-print [val]
+  (println val)
+  val)
+
 ;;; tuneable parameters
 (def topic-weight 3)
 (def min-topic-word-length 4)
 (def topics-to-track 50)
-
-;;; Storage and manipulation of the actual map
-(defn vocabulary [bot irc channel]
-  ;; TODO: Stubbed
-  {::start-sentence {"clojure" 3
-                     "my" 1}
-   "clojure" {"is" 4
-              ::end-sentence 1}
-   "is" {"awesome" 7
-         "clojure" 1}
-   "awesome" {::end-sentence 1}
-   "my" {"favorite" 1}
-   "favorite" {"language" 1}
-   "language" {"is" 1}})
-
-(defn current-topics [bot irc channel]
-  ;; TODO: This is just a stub for dev/testing
-  (set (map str
-            '[clojure
-              macros
-              cheesecake])))
-
-;;; Sentence-creation section
-(defn interest-in [topics]
-  (fn [[word weight]]
-    [word (* weight
-             (if (topics word)
-               topic-weight
-               1))]))
-
-(defn pick-word [vocabulary topics word]
-  (let [nexts (get vocabulary word)
-        [words freqs] (apply map vector (map (interest-in topics) nexts))
-        topical-freqs (map (interest-in topics) freqs)
-        idx (roulette-wheel freqs)
-        res (nth words idx)]
-    (verify (! #{::end-sentence}) res)))
-
-(defn build-sentence [vocabulary topics]
-  (str (->> ::start-sentence
-            (iterate #(pick-word vocabulary topics %))
-            rest
-            trim-seq
-            (string/join " "))
-       "."))
 
 ;;; Parsing section
 (defn word-char? [c]
@@ -99,8 +58,8 @@
 
 (defn replace-with
   "Scans a seq of characters and/or keywords, replacing contiguous groups of
-  objects satisfying pred with the result of (repl seq-of-objects). If repl is
-  not a (fn) object, it is used as a literal replacement object."
+   objects satisfying pred with the result of (repl seq-of-objects). If repl is
+   not a (fn) object, it is used as a literal replacement object."
   [pred repl elts]
   (let [repl (if (fn? repl)
                repl
@@ -114,30 +73,115 @@
 
 (defn tokenize
   "Take an input string and split it into a seq of sentences. Each sentence will
-  be further split into a seq of words."
+   be further split into a seq of words."
   [msg]
   (->> msg
        .toLowerCase
-       (remove ignore?) ; will also convert from str to char seq
+       (remove ignore?)                 ; will also convert from str to char seq
        (replace-with whitespace? ::word-sep) ; placeholder while working with
-                                             ; chars instead of strs
+                                        ; chars instead of strs
        (replace-with sentence-terminator? ::sentence-sep)
        (replace-with char? #(vector (apply str %)))
        (replace-with #{::word-sep} (constantly nil))
        (partition-by #{::sentence-sep})
        (remove #(every? keyword? %))))
 
+(defn links-in [sentence]
+  (partition 2 1
+             (concat [::start-sentence]
+                     sentence
+                     [::end-sentence])))
+
+(defn learn [vocabulary links]
+  (reduce (fn [vocab link]
+            (update-in vocab link (fnil inc 0)))
+          vocabulary
+          links))
+
+(defn look-and-learn [vocabulary tokens]
+  (learn vocabulary (mapcat links-in tokens)))
+
+;;; Storage and manipulation of the actual map
+
+;; TODO This needs to be localized per-irc, maybe per-channel; it should also be
+;; persisted to mongo, but I need some usage data to figure out what the
+;; retention policy should be.
+(def global-vocabulary (atom {::start-sentence {"clojure" 3
+                                                "my" 1}
+                              "clojure" {"is" 4
+                                         ::end-sentence 1}
+                              "is" {"awesome" 7
+                                    "clojure" 1}
+                              "awesome" {::end-sentence 1}
+                              "my" {"favorite" 1}
+                              "favorite" {"language" 1}
+                              "language" {"is" 1}}))
+(def global-topics (atom (map str
+                              '[clojure
+                                macros
+                                cheesecake])))
+
+(defn vocabulary [bot irc channel]
+  @global-vocabulary)
+
+(defn current-topics [bot irc channel]
+  @global-topics)
+
+(defn update-vocab! [bot irc channel tokens]
+  (swap! global-vocabulary look-and-learn tokens))
+
+(defn update-topics! [bot irc channel tokens]
+  (swap! global-topics (comp #(take topics-to-track %)
+                             #(apply concat %1 %2))
+         tokens))
+
+(defn learn-message [bot irc channel msg]
+  (println 'learning msg)
+  (let [tokens (tokenize msg)]
+    (update-vocab! bot irc channel tokens)
+    (update-topics! bot irc channel tokens)))
+
+;;; Sentence-creation section
+(defn interest-in [topics]
+  (fn [[word weight]]
+    [word (* weight
+             (if (some #{word} topics)
+               topic-weight
+               1))]))
+
+(defn pick-word [vocabulary topics word]
+  (let [nexts (get vocabulary word)
+        [words freqs] (apply map vector (map (interest-in topics) nexts))
+        topical-freqs (map (interest-in topics) freqs)
+        idx (roulette-wheel freqs)
+        res (nth words idx)]
+    (verify (! #{::end-sentence}) res)))
+
+(defn build-sentence [vocabulary topics]
+  (str (->> ::start-sentence
+            (iterate #(pick-word vocabulary topics %))
+            rest
+            trim-seq
+            (string/join " ")
+            and-print)
+       "."))
+
 ;;; Plugin mumbo-jumbo
+
+;; TODO add forms of address other than $markov, eg "sexpbot: thoughts?"
 (defplugin
   (:hook
    :on-message
    (fn [{:keys [irc bot nick channel message]}]
-     (comment TODO - add stuff to the database, and maybe
-              add forms of address other than $cmd, eg "sexpbot: thoughts?")))
+     (when-not (or (= nick (:name irc))
+                   (pos? (.indexOf message "markov")))
+       (learn-message bot irc channel message))))
 
   (:cmd
    "Say something that seems to reflect what the channel is talking about."
    #{"markov"}
-   (fn [{:keys [irc bot channel args]}]
+   (fn [{:keys [irc bot channel]}]
      ;; TODO - do something.
-     (send-message irc bot channel (first args)))))
+     (send-message irc bot channel (apply build-sentence
+                                          (map #(% irc bot channel)
+                                               [vocabulary current-topics]))))))
