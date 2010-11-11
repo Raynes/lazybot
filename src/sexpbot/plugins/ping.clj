1b45eb43c8cd48694069a9b0bddde6b9f2093104
Raynes <DiscipleRayne@gmail.com>
2010-11-11 07:03:42 -0600

    Changes to ping. Mostly using clj-time so we can have seconds rather than milliseconds.

--- src/sexpbot/plugins/ping.clj
+++ src/sexpbot/plugins/ping.clj
@@ -1,8 +1,12 @@
 ; Written by Pepijn de Vos <pepijndevos@gmail.com>
 ; Licensed under the EPL
 (ns sexpbot.plugins.ping
-  (:use [sexpbot respond info]
-        [somnium.congomongo :only [fetch fetch-one insert! destroy!]]))
+  (:refer-clojure :exclude [extend])
+  (:use sexpbot.respond
+        [clj-time core format]
+        [somnium.congomongo :only [fetch-one insert! destroy!]]))
+
+(def basic (formatters :basic-date-time))
 
 (defplugin
   (:hook :on-message
@@ -10,11 +14,11 @@
        (when-let [ping (fetch-one :ping :where {:to nick})]
          (destroy! :ping {:to nick})
          (send-message irc bot (:from ping)
-                       (str nick " is available, and took "
-                            (- (System/currentTimeMillis)
-                               (:time ping))
-                            "ms")))
+                       (str nick " is available, "
+                            (in-secs (interval (parse basic (:time ping)) (now)))
+                            " seconds since your ping.")
+                       :notice? true))
        (when-let [[_ to] (re-find #"^(\w+).{1,2}ping!?$" message)]
          (insert! :ping {:from nick
                          :to to
-                         :time (System/currentTimeMillis)})))))
+                         :time (unparse basic (now))})))))
