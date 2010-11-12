d7de85da0af77dc3023aafea24622156d72afc1b
Raynes <DiscipleRayne@gmail.com>
2010-11-12 05:37:26 -0600

    Added some basic background functionality.

--- src/sexpbot/run.clj
+++ src/sexpbot/run.clj
@@ -1,9 +1,21 @@
 (ns sexpbot.run
-  (:use sexpbot.core ring.adapter.jetty)
+  (:use sexpbot.core
+        ring.adapter.jetty
+        clojure.contrib.command-line
+        [clojure.java.io :only [writer]])
   (:gen-class))
 
 (defn -main [& args]
-  (defonce server (run-jetty #'sexpbot.core/sroutes {:port servers-port :join? false}))
-  (require-plugins)
-  (doseq [server (:servers initial-info)] (connect-bot server))
-  (route (extract-routes (vals @bots))))
+  (with-command-line args
+    "sexpbot -- A Clojure IRC bot"
+    [[background? b? "Start sexpbot in the background. Should only be used along with --logpath."]
+     [logpath "A file for sexpbot to direct ouput to."]]
+    (if background?
+      (.exec (Runtime/getRuntime) (str "java -jar sexpbot.jar --logpath " logpath))
+      (let [write (if logpath (writer logpath) *out*)]
+        (alter-var-root #'*out* (fn [& _] write))
+        (alter-var-root #'*err* (fn [& _] write))
+        (defonce server (run-jetty #'sexpbot.core/sroutes {:port servers-port :join? false}))
+        (require-plugins)
+        (doseq [server (:servers initial-info)] (connect-bot server))
+        (route (extract-routes (vals @bots)))))))
