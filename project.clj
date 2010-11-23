5474e44e7620aad9e604b225ea3b3f53bc34be2e
Raynes <DiscipleRayne@gmail.com>
2010-11-22 23:14:13 -0600

    Added clojure-twitter as a dep. Also, added :uberjar-name which will work in future versions of cake.

--- project.clj
+++ project.clj
@@ -8,10 +8,12 @@
                  [clojail "0.1.0"]
 		 [clojure-http-client "1.1.0-SNAPSHOT"]
 		 [irclj "0.4.0-SNAPSHOT"]
+                 [clojure-twitter "1.2.3"]
                  [congomongo "0.1.3-SNAPSHOT"]
 		 [clj-config "0.1.0-SNAPSHOT"]
                  [clj-github "1.0.0-SNAPSHOT"]
                  [compojure "0.4.1"]
                  [ring/ring-jetty-adapter "0.2.5"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
+  :uberjar-name "sexpbot"
   :main sexpbot.run)
