1101b9528c0f3a2175e6de54658eb8b84940de3a
Alan Malloy <alan@malloys.org>
2010-12-11 10:55:15 -0800

    Tidy up the existing code while pulling gosub's $tell command

--- src/sexpbot/plugins/whatis.clj
+++ src/sexpbot/plugins/whatis.clj
@@ -2,47 +2,50 @@
   (:use [sexpbot registry]
         [somnium.congomongo :only [fetch fetch-one insert! destroy!]]))
 
+(defn tell-about [what com-m]
+  (send-message com-m
+                (str what
+                     (if-let [result (fetch-one :whatis :where {:subject what})]
+                       (str " is " (:is result))
+                       " does not exist in my database."))))
+
 (defplugin 
   (:cmd
    "Teaches the bot a new thing. It takes a name and whatever you want to assign the name
    to. For example: $learn me a human being."
    #{"learn"} 
-   (fn [{:keys [bot channel args] :as com-m}]
+   (fn [{:keys [args] :as com-m}]
      (let [[subject & is] args
            is-s (apply str (interpose " " is))]
        (do
          (destroy! :whatis {:subject subject})
          (insert! :whatis {:subject subject :is is-s})
-         (send-message com-m "My memory is more powerful than M-x butterfly. I wont forget it.")))))
+         (send-message com-m "My memory is more powerful than M-x butterfly. I won't forget it.")))))
    
    (:cmd 
     "Pass it a key, and it will tell you what is at the key in the database."
     #{"whatis"}
-    (fn [{:keys [bot channel args] :as com-m}]
-      (if-let [result (fetch-one :whatis :where {:subject (first args)})]
-        (send-message com-m (str (first args) " = " (:is result)))
-        (send-message com-m (str (first args) " does not exist in my database.")))))
+    (fn [{[what] :args :as com-m}]
+      (tell-about what com-m)))
 
    (:cmd 
     "Pass it a key, and it will tell the recipient what is at the key in the database via PM
 Example - $tell G0SUB about clojure"
     #{"tell"}
-    (fn [{:keys [bot channel args] :as com-m}]
-      (when-let [subject (nth args 2 nil)]
-        (if-let [result (fetch-one :whatis :where {:subject subject})]
-          (send-message (assoc com-m :channel (first args)) (str subject " = " (:is result)))
-          (send-message com-m (str subject " does not exist in my database."))))))
+    (fn [{[who _ what] :args :as com-m}]
+      (when what
+        (tell-about what (assoc com-m :channel who)))))
    
    (:cmd 
     "Forgets the value of a key."
     #{"forget"} 
-    (fn [{:keys [bot channel args] :as com-m}]
-      (do (destroy! :whatis {:subject (first args)})
-          (send-message com-m (str "If " (first args) " was there before, it isn't anymore. R.I.P.")))))
+    (fn [{[what] :args :as com-m}]
+      (do (destroy! :whatis {:subject what})
+          (send-message com-m (str "If " what " was there before, it isn't anymore. R.I.P.")))))
 
    (:cmd 
     "Gets a random value from the database."
     #{"rwhatis"} 
-    (fn [{:keys [bot channel] :as com-m}]
-      (let [rand-subject (rand-nth (fetch :whatis))]
-        (send-message com-m (str (:subject rand-subject) " = " (:is rand-subject)))))))
+    (fn [com-m]
+      (let [what (-> :whatis fetch rand-nth :subject)]
+        (tell-about what com-m)))))
