63a233de4e0ec7089ab8a58c6d035e0c91e0ac17
Raynes <i@raynes.me>
2011-01-26 21:37:26 -0600

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sexpbot "0.5.2"
+(defproject sexpbot "0.6.0"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
