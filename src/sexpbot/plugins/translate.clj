e13433bc9ba16d30f3319a6d3026922e33ae5912
Alan Malloy <alan@malloys.org>
2011-04-13 20:44:13 -0700

    Fix translate

--- src/sexpbot/plugins/translate.clj
+++ src/sexpbot/plugins/translate.clj
@@ -1,7 +1,8 @@
 (ns sexpbot.plugins.translate
   (:use [sexpbot registry utilities]
         [clojure.contrib.json :only [read-json]])
-  (:require [clojure-http.resourcefully :as res])
+  (:require [clojure-http.resourcefully :as res]
+            [clojure.string :as s])
   (:import org.apache.commons.lang.StringEscapeUtils))
 
 (defn translate [lang1 lang2 text]
@@ -12,16 +13,16 @@
 
 (defplugin
   (:cmd
-   "Translates with google translate. Takes two language abbreviations (google's ones) and some text
-   to translate, and returns it translated."
-   #{"trans" "translate"}
+   "Translates with google translate. Takes two language
+   abbreviations (google's ones) and some text to translate, and
+   returns it translated."  #{"trans" "translate"}
    (fn [{:keys [bot channel args] :as com-m}]
      (let [[lang-from lang-to & text] args
-           translation (translate lang-from lang-to (stringify text))]
+           translation (translate lang-from lang-to (s/join " " text))]
        (if (:responseData translation)
          (send-message com-m (-> translation 
-                                           :responseData 
-                                           :translatedText 
-                                           StringEscapeUtils/unescapeHtml
-                                           (.replaceAll "\n|\r" "")))
+                                 :responseData 
+                                 :translatedText 
+                                 StringEscapeUtils/unescapeHtml
+                                 (.replaceAll "\n|\r" "")))
          (send-message com-m "Languages not recognized."))))))
