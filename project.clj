ee1159f4694fe498b6c0160da7723739622f9b45
Anthony Grimes <i@raynes.me>
2011-09-08 13:16:44 -0500

    Update some deps.

--- project.clj
+++ project.clj
@@ -4,12 +4,11 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [commons-lang/commons-lang "2.5"]
                  [commons-io/commons-io "1.4"]
-                 [org.clojars.rayne/clj-time "0.1.0-SNAPSHOT"]
+                 [clj-time "0.3.0"]
                  [clojail "0.3.4"]
                  [clojure-http-client "1.1.0-SNAPSHOT"]
                  [irclj "0.4.0-SNAPSHOT"]
-                 [clojure-twitter "1.2.3"]
-                 [congomongo "0.1.3-SNAPSHOT"]
+                 [congomongo "0.1.6"]
                  [clj-config "0.1.0-SNAPSHOT"]
                  [clj-github "1.0.0-SNAPSHOT"]
                  [compojure "0.4.1"]
@@ -22,7 +21,6 @@
                  [amalloy/utils "[0.3.6,)"]
                  [org.thnetos/cd-client "0.3.0"]
                  [org.jruby/jruby "1.6.4"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
   :uberjar-name "lazybot"
   :main lazybot.run
   :copy-deps true
