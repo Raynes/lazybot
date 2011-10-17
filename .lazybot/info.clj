2dfa33fc5d435dc8ffb4be0400731627d66767ca
Alan Malloy <alan@malloys.org>
2011-10-17 12:36:12 -0700

    Add a configurable autoreply plugin

--- .lazybot/info.clj
+++ .lazybot/info.clj
@@ -1,7 +1,7 @@
 (let [plugins #{#_"dictionary" "lmgtfy" "google" "translate" "eball" "utils" "leet" "clojure" "login" "log"
                 "weather" "brainfuck" "whatis" "shorturl" "haskell"
                 "mail" "timer" "fortune" "rss" "title" "operator" "seen" "sed" "help"
-                "load" "embedded" "karma" "markov"}]
+                "load" "embedded" "karma" "markov" "yesno" "autoreply"}]
   {:servers ["irc.freenode.net"]        ; A list of servers.
    :prepends #{"@"}   ; The character you want for a prepend. Currently set to @
    :bitly-login ""    ; Your bit.ly login.
@@ -25,6 +25,7 @@
                        :users {"JohnDoe" {:pass "iliekpie", :privs :admin}
                                "JaneDoe" {:pass "ohai", :privs :admin}}
                        :user-blacklist #{"Meowzorz"}
+                       :autoreplies {"#clojure" {#".*(https?://)richhickey(.github.com/\S*).*" "Nooooo, that's so out of date! Please see instead $1clojure$2 and try to stop linking to rich's repo."}}
                        :catch-links? {true} ; Should only be enabled if the title plugin is activated below.
                        :channel-catch-blacklist #{} ; Channels in which URL title scraper is to be disabled.
                        :url-blacklist #{} ; URL title scraper will look for these words in URLs and not use them if they appear.
