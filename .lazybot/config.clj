ad7f7a57300a1d5ab2617111076dd1225c2c1f92
Anthony Grimes <i@raynes.me>
2011-10-22 02:15:04 -0500

    Updated the configuration file.

--- .lazybot/config.clj
+++ .lazybot/config.clj
@@ -4,23 +4,23 @@
                 "load" "embedded" "karma" "yesno" "autoreply"}]
   {:servers ["irc.freenode.net"]        ; A list of servers.
    :prepends #{"@"}   ; The character you want for a prepend. Currently set to @
-   :bitly-login ""    ; Your bit.ly login.
-   :bitly-key ""      ; API key and login above needed for URL shortening.
-   :dont-sed #{"tomoj" "hiredman"}
-   :wordnik-key ""                      ; API key needed for dictionary access.
+   :weather {:token ""} ; Wunderground token.
+   :dictionary {:wordnik-key "99c266291da87b231f40a0c8902040da0b568588c25526cff"} ; Wordnik API key.
+   :sed {:automatic? true}
    :max-operations 3 ; The maximum number of operations that can be running at any given time.
    :pending-ops 0    ; The number of operations running right now
-   :admin-add? true  ; only admins can add help topics
-   :admin-rm? true   ; only admins can remove help topics
-   :eval-prefixes {:defaults ["->" "." "," ; prefixes in any channel
-                              #"&\|(.*?)(?=\|&|\|&|$)" ; stuff like &|this|&
-                              #"##(([^#]|#(?!#))+)\s*((##)?(?=.*##)|$)"]
-                   ;; list of prefixes NOT to use in certain channels
-                   "#tempchan" ["->"]   ; turn this off for testing
-                   "#clojure" [","]}    ; let clojurebot have this one
-   :servers-port 8080                  ; port for plugins that require webserver
+   :help {:admin-add? true  ; only admins can add help topics
+          :admin-rm? true}   ; only admins can remove help topics
+   :clojure {:eval-prefixes {:defaults ["->" "." "," ; prefixes in any channel
+                                        #"&\|(.*?)(?=\|&|\|&|$)" ; stuff like &|this|&
+                                        #"##(([^#]|#(?!#))+)\s*((##)?(?=.*##)|$)"]
+                             ;; list of prefixes NOT to use in certain channels
+                             "#tempchan" ["->"]   ; turn this off for testing
+                             "#clojure" [","]}}    ; let clojurebot have this one
+   :servers-port 8080                  ; port for plugins that require a webserver
    "irc.freenode.net" {:channels ["#tempchan"]
                        :bot-name "lazybot-test"
+                       :sed {:blacklist #{"#tempchan2"}}
                        :bot-password nil
                        :users {"JohnDoe" {:pass "iliekpie", :privs :admin}
                                "JaneDoe" {:pass "ohai", :privs :admin}}
