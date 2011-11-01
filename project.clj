5a5cc2967a8524ff0bb19610e4b46f52f6baa4a0
Anthony Grimes <i@raynes.me>
2011-10-31 21:05:14 -0500

    Update to latest clojail.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [commons-lang/commons-lang "2.5"]
                  [commons-io/commons-io "1.4"]
                  [backtype/clj-time "0.3.2"]
-                 [clojail "0.4.6-beta4"]
+                 [clojail "0.4.6-beta6"]
                  [clj-http "0.2.1"]
                  [irclj "0.4.1"]
                  [congomongo "0.1.7"]
