2f07b3063353ad8e33b0ace1fbff72ecfc32fe5b
Matt DeBoard <matt.deboard@gmail.com>
2011-11-29 01:52:49 +0000

    Remove redundant code & add some comments.

--- src/lazybot/plugins/github.clj
+++ src/lazybot/plugins/github.clj
@@ -10,9 +10,6 @@
 
 (extend nil Read-JSON-From {:read-json-from (constantly nil)})
 
-(defn join [sep coll]
-  (apply str (interpose sep coll)))
-
 (defn grab-config [] (-> @bots vals first :bot deref :config :github))
 
 (defn format-vec [v]
@@ -21,6 +18,8 @@
               (when (seq hide) "...") "]"])))
 
 (defn notify-chan [com-m commit owner name branch no-header action]
+  "Use data gleaned from (handler) to build alert message to rooms
+specified in config.clj."
   (send-message
    com-m
    (let [{:keys [added removed modified message url]} commit]
@@ -31,9 +30,14 @@
                  " \u0002--\u0002 " (s/replace message #"\n" " ")]))))
 
 (defn handler [req]
+  "Process the JSON payload sent by Github's post-commit hooks."
   (let [remote (:remote-addr req)]
     (when (or (= "127.0.0.1" remote)
-              (.endsWith (.getCanonicalHostName (InetAddress/getByName remote)) "github.com")))
+              (.endsWith (.getCanonicalHostName
+                          (InetAddress/getByName remote))
+                         "github.com")))
+    ;; Though `req` is a proper Clojure map, its :form-params key has a
+    ;; JSON string as its value, which we parse out into a map.
     (let [{:keys [before repository commits after compare ref deleted] :as payload}
           (read-json ((:form-params req) "payload"))
           config (:commits (grab-config))]
@@ -49,6 +53,8 @@
                                 "remotes" {:upper "Remote" :lower "remote"}))
                 n-commits (count commits)
                 branchdel? (true? deleted)
+                ;; If it's a new branch, the "before" field will be a str
+                ;; consisting of a number of zeroes.
                 branchnew? (if (re-find #"^00000+" before) true false)
                 no-header (or (:no-header conf) (= n-commits 1))
                 branch (last (.split ref "/"))]
@@ -62,8 +68,12 @@
                               branchdel? (str (:upper commit-type) " deleted: " branch)
                               branchnew? (str (:upper commit-type) " created: " branch)
                               :else (str (count commits) " new commit(s) on " (:lower commit-type) " " branch ". Compare view at <" (shorten-url compare) ">."))
+                        ;; Only report issues if there are some issues
+                        ;; to report.
                         (if (> (:open_issues repository) 0)
                           (str (:open_issues repository) " open issues remain.")))))
+                ;; Grab first three commits from the message and pass to
+                ;; (notify-chan) to squawk.
                 (doseq [commit (take 3 commits)]
                   (let [action (cond
                                 branchnew? "New branch: "
