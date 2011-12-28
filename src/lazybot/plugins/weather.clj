6c27b853582fa1a36b900f710ddefc077d724f06
Anthony Grimes <i@raynes.me>
2011-12-28 07:50:26 -0600

    Added an astronomy command.

--- src/lazybot/plugins/weather.clj
+++ src/lazybot/plugins/weather.clj
@@ -1,6 +1,7 @@
 (ns lazybot.plugins.weather
-  (:use ororo.core
-        [lazybot registry [utilities :only [shorten-url]]]))
+  (:use [lazybot registry [utilities :only [shorten-url]]])
+  (:require [ororo.core :as w]
+            [clojure.string :as string]))
 
 (defn token [bot] (-> bot :config :weather :token))
 
@@ -12,15 +13,33 @@
 (defn generate-forecast [m]
   (str (:title m) ": " (:fcttext m)))
 
+(defn extract-time [k]
+  (fn [data]
+    (string/join ":" ((juxt :hour :minute) (k data)))))
+
 (defplugin
   (:cmd
    "Get the forecast for a location."
    #{"forecast"}
    (fn [{:keys [bot nick args] :as com-m}]
      (when-let [token (token @bot)]
-       (doseq [msg (->> (forecast token (parse-location args))
+       (doseq [msg (->> (w/forecast token (parse-location args))
                         :txt_forecast
                         :forecastday
                        (map generate-forecast))]
          (send-message (assoc com-m :channel nick) msg)
-         (Thread/sleep 1000))))))
+         (Thread/sleep 1000)))))
+
+  (:cmd
+   "Get astronomy info for a location."
+   #{"astronomy"}
+   (fn [{:keys [bot nick args] :as com-m}]
+     (when-let [token (token @bot)]
+       (let [data (w/astronomy token (parse-location args))]
+         (send-message
+          com-m
+          (apply format
+                 (str "Percentage of moon illuminated: %s; Age of moon: %s; "
+                      "Current time: %s; Sunset: %s; Sunrise: %s.")
+                 ((juxt :percentIlluminated :ageOfMoon (extract-time :current_time)
+                        (extract-time :sunset) (extract-time :sunrise)) data))))))))
