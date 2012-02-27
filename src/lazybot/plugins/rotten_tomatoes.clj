b73c7815610919428b0d90067601f49357405a2c
Anthony Grimes <i@raynes.me>
2012-02-27 03:43:57 -0600

    Add a command to see the highest rated movies currently playing.

--- src/lazybot/plugins/rotten_tomatoes.clj
+++ src/lazybot/plugins/rotten_tomatoes.clj
@@ -11,7 +11,7 @@
     (let [{:strs [title year critics_consensus ratings links]} movie
           score (ratings "critics_score")
           link (links "alternate")]
-      (format "%s -- %s | rating: %s | consensus: %s | link: %s"
+      (format "%s - %s | rating: %s | consensus: %s | link: %s"
               year title score critics_consensus link))))
 
 (defn get-ratings [key query]
@@ -26,6 +26,22 @@
     first
     compose-message))
 
+(defn rating [movie]
+  (let [rating (get-in movie ["ratings" "critics_score"])]
+    (when rating
+      (Integer. rating))))
+
+(defn in-theaters [key]
+  (-> (http/get (str base "lists/movies/in_theaters.json")
+                {:query-params {:apikey key}})
+    :body
+    json/parse-string
+    (get "movies")
+    (->>
+      (sort-by rating)
+      reverse
+      (map compose-message))))
+
 (defplugin
   (:cmd
     "Get the rotten tomatoes rating for a movie."
@@ -36,4 +52,13 @@
                       (get-ratings
                         (get-in @bot [:config :rotten-tomatoes :key])
                         (string/join " " args))
-                      "Movie not found.")))))
+                      "Movie not found."))))
+  
+  (:cmd
+    "Get a list of the top 5 highest rated movies currently in theaters."
+    #{"playing"}
+    (fn [{:keys [bot nick args] :as com-m}]
+      (doseq [message (take (or (when (seq args) (Integer. (first args))) 5)
+                            (in-theaters
+                              (get-in @bot [:config :rotten-tomatoes :key])))]
+        (send-message (assoc com-m :channel nick) message :notice? true)))))
