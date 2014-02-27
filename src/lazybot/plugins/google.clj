1f0d8da6535e703bc87381b51705463f30d71ce9
Kyle Kingsbury <aphyr@aphyr.com>
2014-02-27 15:25:28 -0800

    Add a google image search command: @gis
    Returns a random image from the google results for args.

--- src/lazybot/plugins/google.clj
+++ src/lazybot/plugins/google.clj
@@ -7,33 +7,67 @@
   (:import org.apache.commons.lang.StringEscapeUtils
            java.net.URLDecoder))
 
-(defn google [term]
-  (-> (http/get "http://ajax.googleapis.com/ajax/services/search/web"
-                {:query-params {"v" "1.0" "q" term}})
+(defn google [service term]
+  "Services: \"web\", \"images\""
+  (-> (http/get (str "http://ajax.googleapis.com/ajax/services/search/"
+                     service)
+                {:query-params {"v"  "1.0"
+                                "rsz" 8 ; 8 results
+                                "q"   term}})
       :body
       parse-string))
 
-(defn handle-search [{:keys [args] :as com-m}]
+(defn search-string
+  "From an argument list, builds a search string."
+  [args]
+  (->> args
+       (s/join " ")
+       s/trim))
+
+(defn handle-search [com-m]
   (send-message com-m
-                (let [argstr (s/join " " args)]
-                  (if-not (seq (s/trim argstr))
-                    (str "No search term!")
-                    (let [results (google argstr)
-                          {:strs [url titleNoFormatting]} (first (get-in results ["responseData" "results"]))
-                          res-count (get-in results ["responseData" "cursor" "estimatedResultCount"])]
+                (let [q (search-string (:args com-m))]
+                  (if-not (seq q)
+                    (str "No search terms!")
+                    (let [results (google "web" q)
+                          {:strs [url titleNoFormatting]}
+                            (first (get-in results ["responseData" "results"]))
+                          res-count (get-in results ["responseData"
+                                                     "cursor"
+                                                     "estimatedResultCount"])]
                       (if (and results url)
                         (str "["
                              (trim-string 80 (constantly "...")
-                                          (StringEscapeUtils/unescapeHtml titleNoFormatting))
+                                          (StringEscapeUtils/unescapeHtml
+                                            titleNoFormatting))
                              "] "
                              (URLDecoder/decode url "UTF-8"))))))))
 
+(defn handle-image-search
+  "Finds a random google image for a string, and responds with the URI."
+  [com-m]
+  (send-message com-m
+                (let [q (search-string (:args com-m))]
+                  (if-not (seq q)
+                    (str "No search terms!")
+                    (-> (google "images" q)
+                        (get "responseData")
+                        (get "results")
+                        rand-nth
+                        (get "url")
+                        (URLDecoder/decode "UTF-8"))))))
+
 (defplugin
   (:cmd
-   "Searches google for whatever you ask it to, and displays the first result and the estimated
-   number of results found."
+   "Searches google for whatever you ask it to, and displays the first result
+   and the estimated number of results found."
    #{"google"}
    #'handle-search)
+
+  (:cmd
+    "Searches google for a string, and returns a random image from the results."
+    #{"gis"}
+    #'handle-image-search)
 
   (:cmd
    "Searches wikipedia via google."
